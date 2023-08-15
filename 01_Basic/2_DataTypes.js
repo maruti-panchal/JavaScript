c1721dc4ef38fefeda1f3ef3aa1c62b2f5714972
maruti-panchal <112648935+maruti-panchal@users.noreply.github.com>
2023-08-15 14:50:28 +0000

    Learnd about datatypes

---
+++ 01_Basic/2_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// Number 
+let length=16;
+let weight=34.5;
+console.log(length,weight);
+console.log(typeof length);
+console.log("******************************");
+// string
+let fName="Maruti";
+let lName="Panchal";
+console.log(fName+" "+lName);
+console.log(typeof (fName+" "+lName));
+console.log("******************************");
+//boolean
+let x=true;
+let y=false;
+console.log(x);
+console.log(y);
+console.log(typeof(x,y));
+console.log("******************************");
+// Undefined
+// null 
