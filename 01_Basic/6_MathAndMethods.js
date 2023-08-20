3e90eb0a1553a01cf5b245eea7079e7e8ded8678
maruti-panchal <112648935+maruti-panchal@users.noreply.github.com>
2023-08-20 11:31:51 +0000

    learned ceil,floor,round,trunc,max,min methods.

---
+++ 01_Basic/6_MathAndMethods.js
@@ -0,0 +1,72 @@
+/* Math Methods
+==> ceil(X):- It will gives the upper value og it's nearest Integer.
+==> floor(x):- It will return the floor value.
+==> round(x):- This method rounds a number to the nearest integer.
+==> trunc(x):- This method returns the integer part of a number and discard the decimal values.
+==> sqrt(x):- 
+==> cbrt(x):- 
+==> pow(x,y):-
+==> random():-
+==> abs(x):-
+==> PI :- 
+==> max(x,y,z,...,n):- This method returns the highest value.
+==> min(x,y,z,...,n):- This method return the lowest value.
+*/
+
+// # ceil(x)
+/*
+const a=Math.ceil(5.2);
+console.log(a);  //  6
+const b=Math.ceil(5.70);
+console.log(b);  //  6
+const c=Math.ceil(-5.70);
+console.log(c);  // -5
+*/
+
+// # floor(x)
+/*
+const a=Math.floor(5.2);
+console.log(a);  //  5
+const b=Math.floor(5.70);
+console.log(b);  //  5
+const c=Math.floor(-5.70);
+console.log(c);  // -6
+const d=Math.floor(-6.70);
+console.log(d);  // -7
+*/
+
+// # round(x)
+/*
+const a=6.20;
+const b=6.90;
+console.log(Math.round(a));   // 6
+console.log(Math.round(b));   // 7
+*/
+
+// # trunc(x)
+/*
+const a= 2.49;
+const b=2.98;
+const c=-2.49;
+const d=-2.94;
+console.log(Math.trunc(a));   //  2
+console.log(Math.trunc(b));   //  2
+console.log(Math.trunc(c));   // -2
+console.log(Math.trunc(d));   // -2
+*/
+
+// # max()
+/*
+const a=Math.max(1,2,3,4,5,6,7,8,9);
+console.log(a);  // 9
+const b=Math.max(-1,-2,-3,-4,-5,-6,-7,-8,-9);
+console.log(b);  // -1
+*/
+
+// # min()
+/*
+const a=Math.min(1,2,3,4,5,6,7,8,9);
+console.log(a);  // 1
+const b=Math.min(-1,-2,-3,-4,-5,-6,-7,-8,-9);
+console.log(b);   // -9
+*/
