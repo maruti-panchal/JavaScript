6f38bd0a2330a7ddf39efbd464a0f0cd502b5325
maruti-panchal <112648935+maruti-panchal@users.noreply.github.com>
2023-08-20 12:10:12 +0000

    final touch

--- 01_Basic/6_MathAndMethods.js
+++ 01_Basic/6_MathAndMethods.js
@@ -96,6 +96,9 @@ console.log(Math.random());     // 0.5836423144057297
 console.log(Math.random());    // 0.05913645457709116
 console.log(Math.random());   // 0.8904589981527231
 console.log(Math.floor(Math.random()*10)+1);     // generate random number between 1 to 10
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);  // 10-20
 */
 
 // # abs()
