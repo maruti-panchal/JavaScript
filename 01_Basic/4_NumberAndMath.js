994811a42be91ae9fc4fe1b7ec343d3de85b380b
maruti-panchal <112648935+maruti-panchal@users.noreply.github.com>
2023-08-20 09:48:30 +0000

    learned Number

---
+++ 01_Basic/4_NumberAndMath.js
@@ -0,0 +1,27 @@
+/*
+# Number methods
+==> Number() :- It will convert in number type.
+==> parseInt() :- It is same as number.It will convert inrto strict integer.
+==> parseFloat() :- It is same as number.it will convert into decimal value.
+==> isFinite()
+==> isInteger()
+==> toFixed(x)
+==> toPrecision(x) 
+*/
+
+// // #Number() 
+// const a="10";
+// const b=10;
+// console.log(typeof a);             // string
+// console.log(typeof b);            // number
+// const c=a+b;
+// console.log(c);                 // 1010
+// console.log(typeof c);         // string
+// const d=Number(a);
+// console.log(d);              //10
+// console.log(typeof d);      //number 
+// const e=d+b;
+// console.log(e);           // 20
+// console.log(typeof e);   // number
+
+
