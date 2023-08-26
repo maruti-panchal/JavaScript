0900358e0551dfc3713731514977e3586e01e5f3
maruti-panchal <112648935+maruti-panchal@users.noreply.github.com>
2023-08-26 12:31:33 +0000

    arrow function

---
+++ 04_Functions/02_arrow_function.js
@@ -0,0 +1,15 @@
+ const result=(a,b)=>a+b;
+ console.log(result(5,5));
+ const result1=(a,b,c)=>a*b*c;
+ console.log(result1(10,10,10));
+
+ const calculator=(x,y)=>{
+    console.log(x+y);
+    console.log(x-y);
+    console.log(x*y);
+    console.log(x/y);
+    console.log(x%y);
+ }
+
+
+calculator(10,3);
